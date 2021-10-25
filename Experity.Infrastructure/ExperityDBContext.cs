ca4987b9ff52a91a5d3e02b6c030d96b70c6234b
narouzmakar <narouz.makar@gmail.com>
2021-10-25 08:50:51 -0500

    clean code

--- Experity.Infrastructure/ExperityDBContext.cs
+++ Experity.Infrastructure/ExperityDBContext.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 namespace Experity.Infrastructure
 {
     class ExperityDBContext: DbContext
     {
         private readonly string connectionString;
-
-        //public static readonly ILoggerFactory ConsoleLoggerFactory = LoggerFactory.Create(builder => { builder.AddFilter(); });
         public ExperityDBContext(string connectionString)
         {
             this.connectionString = connectionString;
@@ -21,7 +18,7 @@ namespace Experity.Infrastructure
                 maxRetryCount: 10,
                 maxRetryDelay: TimeSpan.FromSeconds(30),
                 errorNumbersToAdd: null);
-            });//.UseLowerCaseNamingConvention().UseLazyLoadingProxies();
+            });
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
